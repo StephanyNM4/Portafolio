1e98f7e20e166788e4c893abc82b02ab877ef5b5
StephanyNM4 <stephanymatamoros4@gmail.com>
2023-08-27 16:52:19 -0700

    Index

---
+++ assets/js/controlador.js
@@ -0,0 +1,174 @@
+const mostrarLanding = () => {
+    document.getElementById('contenedor-principal').style.display = 'block';
+    document.getElementById('navegador').style.display = 'block';
+    document.getElementById('informacion').style.display = 'none';
+    document.getElementById('certificados').style.display = 'none';
+    document.getElementById('baseDatos').style.display = 'none';
+    document.getElementById('servicioCliente').style.display = 'none';
+    document.getElementById('competenciasLaborales').style.display = 'none';
+    document.getElementById('database').style.display = 'none';
+    document.getElementById('neuromarketing').style.display = 'none';
+    document.getElementById('orientacionSociolaboral').style.display = 'none';
+    document.getElementById('photoshop').style.display = 'none';
+    document.getElementById('tecnicaVentas').style.display = 'none';
+
+
+}
+
+const mostrarInformacion = () => {
+    document.getElementById('contenedor-principal').style.display = 'none';
+    document.getElementById('navegador').style.display = 'block';
+    document.getElementById('informacion').style.display = 'block';
+    document.getElementById('certificados').style.display = 'none';
+    document.getElementById('baseDatos').style.display = 'none';
+    document.getElementById('servicioCliente').style.display = 'none';
+    document.getElementById('competenciasLaborales').style.display = 'none';
+    document.getElementById('database').style.display = 'none';
+    document.getElementById('neuromarketing').style.display = 'none';
+    document.getElementById('orientacionSociolaboral').style.display = 'none';
+    document.getElementById('photoshop').style.display = 'none';
+    document.getElementById('tecnicaVentas').style.display = 'none';
+
+}
+
+const mostrarCertificados = () => {
+    document.getElementById('contenedor-principal').style.display = 'none';
+    document.getElementById('navegador').style.display = 'block';
+    document.getElementById('informacion').style.display = 'none';
+    document.getElementById('certificados').style.display = 'block';
+    document.getElementById('baseDatos').style.display = 'none';
+    document.getElementById('servicioCliente').style.display = 'none';
+    document.getElementById('competenciasLaborales').style.display = 'none';
+    document.getElementById('database').style.display = 'none';
+    document.getElementById('neuromarketing').style.display = 'none';
+    document.getElementById('orientacionSociolaboral').style.display = 'none';
+    document.getElementById('photoshop').style.display = 'none';
+    document.getElementById('tecnicaVentas').style.display = 'none';
+
+}
+
+const servicioCliente = () => {
+    document.getElementById('contenedor-principal').style.display = 'none';
+    document.getElementById('navegador').style.display = 'block';
+    document.getElementById('informacion').style.display = 'none';
+    document.getElementById('certificados').style.display = 'none';
+    document.getElementById('baseDatos').style.display = 'none';
+    document.getElementById('servicioCliente').style.display = 'block';
+    document.getElementById('competenciasLaborales').style.display = 'none';
+    document.getElementById('database').style.display = 'none';
+    document.getElementById('neuromarketing').style.display = 'none';
+    document.getElementById('orientacionSociolaboral').style.display = 'none';
+    document.getElementById('photoshop').style.display = 'none';
+    document.getElementById('tecnicaVentas').style.display = 'none';
+
+}
+
+const baseDatos = () => {
+    document.getElementById('contenedor-principal').style.display = 'none';
+    document.getElementById('navegador').style.display = 'block';
+    document.getElementById('informacion').style.display = 'none';
+    document.getElementById('certificados').style.display = 'none';
+    document.getElementById('baseDatos').style.display = 'block';
+    document.getElementById('servicioCliente').style.display = 'none';
+    document.getElementById('competenciasLaborales').style.display = 'none';
+    document.getElementById('database').style.display = 'none';
+    document.getElementById('neuromarketing').style.display = 'none';
+    document.getElementById('orientacionSociolaboral').style.display = 'none';
+    document.getElementById('photoshop').style.display = 'none';
+    document.getElementById('tecnicaVentas').style.display = 'none';
+
+}
+
+const competenciasLaborales = () => {
+    document.getElementById('contenedor-principal').style.display = 'none';
+    document.getElementById('navegador').style.display = 'block';
+    document.getElementById('informacion').style.display = 'none';
+    document.getElementById('certificados').style.display = 'none';
+    document.getElementById('baseDatos').style.display = 'none';
+    document.getElementById('servicioCliente').style.display = 'none';
+    document.getElementById('competenciasLaborales').style.display = 'block';
+    document.getElementById('database').style.display = 'none';
+    document.getElementById('neuromarketing').style.display = 'none';
+    document.getElementById('orientacionSociolaboral').style.display = 'none';
+    document.getElementById('photoshop').style.display = 'none';
+    document.getElementById('tecnicaVentas').style.display = 'none';
+
+}
+
+const database = () => {
+    document.getElementById('contenedor-principal').style.display = 'none';
+    document.getElementById('navegador').style.display = 'block';
+    document.getElementById('informacion').style.display = 'none';
+    document.getElementById('certificados').style.display = 'none';
+    document.getElementById('baseDatos').style.display = 'none';
+    document.getElementById('servicioCliente').style.display = 'none';
+    document.getElementById('competenciasLaborales').style.display = 'none';
+    document.getElementById('database').style.display = 'block';
+    document.getElementById('neuromarketing').style.display = 'none';
+    document.getElementById('orientacionSociolaboral').style.display = 'none';
+    document.getElementById('photoshop').style.display = 'none';
+    document.getElementById('tecnicaVentas').style.display = 'none';
+
+}
+
+const neuromarketing = () => {
+    document.getElementById('contenedor-principal').style.display = 'none';
+    document.getElementById('navegador').style.display = 'block';
+    document.getElementById('informacion').style.display = 'none';
+    document.getElementById('certificados').style.display = 'none';
+    document.getElementById('baseDatos').style.display = 'none';
+    document.getElementById('servicioCliente').style.display = 'none';
+    document.getElementById('competenciasLaborales').style.display = 'none';
+    document.getElementById('database').style.display = 'none';
+    document.getElementById('neuromarketing').style.display = 'block';
+    document.getElementById('orientacionSociolaboral').style.display = 'none';
+    document.getElementById('photoshop').style.display = 'none';
+    document.getElementById('tecnicaVentas').style.display = 'none';
+
+}
+
+const orientacionSociolaboral = () => {
+    document.getElementById('contenedor-principal').style.display = 'none';
+    document.getElementById('navegador').style.display = 'block';
+    document.getElementById('informacion').style.display = 'none';
+    document.getElementById('certificados').style.display = 'none';
+    document.getElementById('baseDatos').style.display = 'none';
+    document.getElementById('servicioCliente').style.display = 'none';
+    document.getElementById('competenciasLaborales').style.display = 'none';
+    document.getElementById('database').style.display = 'none';
+    document.getElementById('neuromarketing').style.display = 'none';
+    document.getElementById('orientacionSociolaboral').style.display = 'block';
+    document.getElementById('photoshop').style.display = 'none';
+    document.getElementById('tecnicaVentas').style.display = 'none';
+
+}
+
+const photoshop = () => {
+    document.getElementById('contenedor-principal').style.display = 'none';
+    document.getElementById('navegador').style.display = 'block';
+    document.getElementById('informacion').style.display = 'none';
+    document.getElementById('certificados').style.display = 'none';
+    document.getElementById('baseDatos').style.display = 'none';
+    document.getElementById('servicioCliente').style.display = 'none';
+    document.getElementById('competenciasLaborales').style.display = 'none';
+    document.getElementById('database').style.display = 'none';
+    document.getElementById('neuromarketing').style.display = 'none';
+    document.getElementById('orientacionSociolaboral').style.display = 'none';
+    document.getElementById('photoshop').style.display = 'block';
+    document.getElementById('tecnicaVentas').style.display = 'none';
+}
+
+const tecnicaVentas = () => {
+    document.getElementById('contenedor-principal').style.display = 'none';
+    document.getElementById('navegador').style.display = 'block';
+    document.getElementById('informacion').style.display = 'none';
+    document.getElementById('certificados').style.display = 'none';
+    document.getElementById('baseDatos').style.display = 'none';
+    document.getElementById('servicioCliente').style.display = 'none';
+    document.getElementById('competenciasLaborales').style.display = 'none';
+    document.getElementById('database').style.display = 'none';
+    document.getElementById('neuromarketing').style.display = 'none';
+    document.getElementById('orientacionSociolaboral').style.display = 'none';
+    document.getElementById('photoshop').style.display = 'none';
+    document.getElementById('tecnicaVentas').style.display = 'block';
+}
